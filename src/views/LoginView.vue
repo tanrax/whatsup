3980a7749c35f5898dbe6b3e25803bdca36512b6
Andros Fenollosa <andros@fenollosa.email>
2022-03-07 11:19:35 +0100

    Add sass

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    <h1>Identificate</h1>
+    <p>¿No tienes cuenta? <router-link :to="{ 'name': 'signup' }">Regístrate</router-link>.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  components: {
+  }
+}
+</script>
---
+++ src/views/SignupView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="signup">
+    <h1>Registro</h1>
+    <section>
+      <form class="signup__form">
+        <label class="signup__label">
+          Email
+          <input class="signup__input" type="email" name="email" placeholder="Tu correo...">
+          <span class="signup__error">No contiene un formato valido</span>
+        </label>
+        <label class="signup__label">
+          Nombre
+          <input class="signup__input" type="text" name="nombre" placeholder="Tu nombre...">
+          <span class="signup__error">Campo obligatorio</span>
+        </label>
+        <label class="signup__label">
+          Contraseña
+          <input class="signup__input" type="password" name="password">
+        </label>
+        <label class="signup__label">
+          Repìte tu contraseña
+          <input class="signup__input" type="password" name="password-repeat">
+          <span class="signup__error">Las contraseñas son obligatorias y deben coincidir</span>
+        </label>
+        <p>
+          <input class="signup__input" type="submit" value="Registrarme">
+        </p>
+      </form>
+    </section>
+    <section>
+      <p>
+        <router-link :to="{ 'name': 'login' }">Identificarme</router-link>.
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignupView',
+  components: {
+  }
+}
+</script>
